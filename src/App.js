365de67d5d843ae021201243ee2d913047b2cc99
Mario Martinez <amakenadog@gmail.com>
2020-12-15 11:24:07 -0600

    First commit - no formatter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Person from "./person";
 
 function App() {
+  const petList = [
+    {
+      id: 1,
+      name: "Hotch",
+      isAlive: false
+    },
+    {
+      id: 2,
+      name: "Bonita",
+      isAlive: false
+    },
+    {
+      id: 3,
+      name: "Peluso",
+      isAlive: false
+    },
+    {
+      id: 4,
+      name: "Nami",
+      isAlive: true
+    },
+    {
+      id: 5,
+      name: "Lola",
+      isAlive: false
+    }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Trying...
         </p>
+        <Person />
+        <Calculator number1={1} number2={2} />
+        <Pets pets={petList} />
+        <Hello />
+        <Hello name="Ada" />
+        <Hello name="Wong" />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +56,42 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Hello({ name = "World!" }) {
+  return <p>Hello {name}</p>;
+}
+
+function Pets({ pets }) {
+  return (
+    <ul>
+      {pets.map(pet => (
+        <Pet key={pet.id} {...pet} />
+      ))}
+    </ul>
+  );
+}
+
+function Pet({ id, name, isAlive }) {
+  return (
+    <li className={isAlive ? "is-alive" : ""}>
+      {name} - has id {id} and is {isAlive ? "alive" : "not alive :("}
+    </li>
+  );
+}
+
+function Calculator({ number1, number2 }) {
+  if (!number1) {
+    return "number1 is required";
+  }
+
+  if (!number2) {
+    return "number2 is required";
+  }
+
+  const result = number1 + number2;
+
+  return <p>{result}</p>;
 }
 
 export default App;
---
+++ src/person/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export default function Person({}) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+        const payload = { firstName, lastName, email };
+
+        console.log(payload);
+
+    event.target.reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First name</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="lastName">Last name</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+}
